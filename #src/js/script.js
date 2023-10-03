acfdc1d9b8dd90891113bfa865e87cc947bd77e8
IlyaRaduk <117042097+IlyaRaduk@users.noreply.github.com>
2023-10-03 13:18:20 +0300

    init

---
+++ #src/js/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // const btnsArr = document.querySelectorAll('.info__btn');
+    // btnsArr.forEach(e => {
+    //     e.addEventListener('click', (el) => {
+    //         const footer = el.target.closest('.bonus').querySelector('.footer');
+    //         const btn = el.target.closest('.info__btn');
+    //         const btnText = btn.querySelector('span');
+
+    //         if (footer.classList.contains('footer_active')) {
+    //             footer.classList.remove('footer_active');
+    //             btn.classList.remove('info__btn_active');
+    //             btnText.innerHTML = 'More info';
+    //         }
+    //         else {
+    //             footer.classList.add('footer_active');
+    //             btn.classList.add('info__btn_active');
+    //             btnText.innerHTML = 'Hide info';
+    //         }
+    //     })
+    // })
+});
